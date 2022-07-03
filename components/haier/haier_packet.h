b03506b6d0b68dbc5419e8f30a4588d49bdb55a5
Pavlo Dudnytskyi <paveldudn@gmail.com>
2022-07-03 11:44:21 +0200

    Some packet fields clarified based on loggigng captured

--- components/haier/haier_packet.h
+++ components/haier/haier_packet.h
@@ -56,39 +56,54 @@ struct HaierPacketControl
                 uint8_t             unused_1:4;
     /* 12 */    uint8_t             fan_mode:4;                 // See enum FanMode
                 uint8_t             ac_mode:4;                  // See enum ConditioningMode
-    /* 13 */    uint8_t             unknown_1;
-    /* 14 */    uint8_t             away_mode:1;                //away mode for 10c
-                uint8_t             display_enabled:1;          // if the display is on or off
-                uint8_t             unknown_2:3;
-                uint8_t             use_fahrenheit:1;           // use Fahrenheit instead of Celsius
-                uint8_t             unknown_3:1;
+    /* 13 */    uint8_t             unknown_1;                  // ??? windSpeed
+    /* 14 */    uint8_t             away_mode:1;                // Away mode for 10c
+                uint8_t             display_enabled:1;          // If the display is on or off
+                uint8_t             use_half_degree:1;          // Use half degree value
+                uint8_t             intelligence_status:1;      // ??? Intelligence status
+                uint8_t             pmv_status:1;               // ??? Pulse Motor Valve status
+                uint8_t             use_fahrenheit:1;           // Use Fahrenheit instead of Celsius
+                uint8_t             unknown_3:1;                // ??? energySavePeriod
                 uint8_t             self_clean_56:1;            // Self cleaning (56°C Steri-Clean)
     /* 15 */    uint8_t             ac_power:1;                 // Is ac on or off
-                uint8_t             purify_1:1;                 // Purify mode (can be related to bits of byte 21)
-                uint8_t             unknown_4:1;
+                uint8_t             health_mode:1;              // Health mode on or off
+                uint8_t             electric_heating_status:1;  // Electric heating status
                 uint8_t             fast_mode:1;                // Fast mode
                 uint8_t             quiet_mode:1;               // Quiet mode
                 uint8_t             sleep_mode:1;               // Sleep mode
                 uint8_t             lock_remote:1;              // Disable remote
-                uint8_t             unknown_5:1;
-    /* 16 */    uint8_t             unknown_6;
+                uint8_t             echo_status:1;              // ??? Echo status
+    /* 16 */    uint8_t             target_humidity;            // ??? Probably target humidity with with 30% offset
     /* 17 */    uint8_t             horizontal_swing_mode:4;    // See enum HorizontalSwingMode
-                uint8_t             unused_2:4;
-    /* 18 */    uint8_t             unknown_7;
-    /* 19 */    uint8_t             purify_2:1;                 // Purify mode
-                uint8_t             unknown_8:1;
-                uint8_t             purify_3:2;                 // Purify mode
-                uint8_t             self_clean:1;               // Self cleaning (not 56°C)
-                uint8_t             unknown_9:3;
+                uint8_t             unused_2:4;                 // ??? Human sensing status
+    /* 18 */    uint8_t             unknown_7;                  // ??? Probably unused
+    /* 19 */    uint8_t             fresh_air_status:1;         // ??? Fresh air status
+                uint8_t             humidification_status:1;    // ??? Humidification status
+                uint8_t             pm2p5_cleaning_status:1;    // ??? PM2.5 cleaning status
+                uint8_t             ch20_cleaning_status:1;     // ??? CH2O cleaning status
+                uint8_t             self_cleaning_status:1;     // ??? Self cleaning status
+                uint8_t             light_status:1;             // ??? Light status
+                uint8_t             energy_saving_status:1;     // ??? Energy saving status
+                uint8_t             change_filter:1;            // ??? Filter need replacement
 };
 
 struct HaierPacketSensors
 {
     /* 20 */    uint8_t             room_temperature;           // 0.5 °C step
-    /* 21 */    uint8_t             unknown_1;                  // always 0 (Room humidity for devices that support it ?)
+    /* 21 */    uint8_t             room_humidity;              // ???
     /* 22 */    uint8_t             outdoor_temperature;        // 0.5 °C step
-    /* 23 */    uint8_t             unknown_2[2];               // unknown (Outdoor humidity for devices that support it + something else ?)
-    /* 25 */    uint8_t             compressor;                 // seems to be 1 in off, 3 in heat? changeover valve?
+    /* 23 */    uint8_t             unknown_1[2];               // unknown (acType (probably 1 byte),
+                                                                // probably capabilities mask (each field 1 bit): sensingResult, airQuality, pm2p5Level, errCode, ErrAckFlag, operationModeHK)
+    /* 25 */    uint8_t             operation_source;           // who is controlling AC (1 - remote, 3 - ESP)
+    // Following bytes should have next fields (16 bytes):
+    //      totalCleaningTime       (probably in seconds 4 bytes or minutes 2 bytes)
+    //      indoorPM2p5Value        (probably 2 bytes)
+    //      outdoorPM2p5Value       (probably 2 bytes)
+    //      ch2oValue               (probably 2 bytes)
+    //      vocValue                (probably 2 bytes)
+    //      co2Value                (probably 2 bytes)
+    //      totalElectricityUsed    (probably 4 bytes)
+    // My AC don't support them (always 0) so no way to figure out how they encoded
 };
 
 struct HaierStatus
