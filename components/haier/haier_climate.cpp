827a8df09900d7cab12375c45caf9d2589a3a9b4
paveldn <paveldudn@gmail.com>
2022-12-08 12:40:44 +0100

    0x55 problem fix 3

--- components/haier/haier_climate.cpp
+++ components/haier/haier_climate.cpp
@@ -23,9 +23,9 @@ namespace haier {
 
 #define MAX_MESSAGE_SIZE                254
 #define MAX_FRAME_SIZE                  (MAX_MESSAGE_SIZE + 4)
-#define SEPARATOR_BYTE					0xFF
-#define SEPARATOR_POST_BYTE				0x55
-#define CRC_INIT_VAL					0x00
+#define SEPARATOR_BYTE                  0xFF
+#define SEPARATOR_POST_BYTE             0x55
+#define CRC_INIT_VAL                    0x00
 
 // ESP_LOG_LEVEL don't work as I want it so I implemented this macro
 #define ESP_LOG_L(level, tag, format, ...) do {                     \
@@ -41,65 +41,65 @@ namespace haier {
 
 uint8_t getChecksum(const uint8_t * message, size_t size) 
 {
-	uint8_t result = 0;
-	for (int i = 0; i < size; i++){
-		result += message[i];
-	}
-	return result;
+    uint8_t result = 0;
+    for (int i = 0; i < size; i++){
+        result += message[i];
+    }
+    return result;
 }
 
 // CRC-16/ARC lookup table
 static const uint16_t crc_table[] = {
-		0x0000, 0xC0C1, 0xC181, 0x0140, 0xC301, 0x03C0, 0x0280, 0xC241,
-		0xC601, 0x06C0, 0x0780, 0xC741, 0x0500, 0xC5C1, 0xC481, 0x0440,
-		0xCC01, 0x0CC0, 0x0D80, 0xCD41, 0x0F00, 0xCFC1, 0xCE81, 0x0E40,
-		0x0A00, 0xCAC1, 0xCB81, 0x0B40, 0xC901, 0x09C0, 0x0880, 0xC841,
-		0xD801, 0x18C0, 0x1980, 0xD941, 0x1B00, 0xDBC1, 0xDA81, 0x1A40,
-		0x1E00, 0xDEC1, 0xDF81, 0x1F40, 0xDD01, 0x1DC0, 0x1C80, 0xDC41,
-		0x1400, 0xD4C1, 0xD581, 0x1540, 0xD701, 0x17C0, 0x1680, 0xD641,
-		0xD201, 0x12C0, 0x1380, 0xD341, 0x1100, 0xD1C1, 0xD081, 0x1040,
-		0xF001, 0x30C0, 0x3180, 0xF141, 0x3300, 0xF3C1, 0xF281, 0x3240,
-		0x3600, 0xF6C1, 0xF781, 0x3740, 0xF501, 0x35C0, 0x3480, 0xF441,
-		0x3C00, 0xFCC1, 0xFD81, 0x3D40, 0xFF01, 0x3FC0, 0x3E80, 0xFE41,
-		0xFA01, 0x3AC0, 0x3B80, 0xFB41, 0x3900, 0xF9C1, 0xF881, 0x3840,
-		0x2800, 0xE8C1, 0xE981, 0x2940, 0xEB01, 0x2BC0, 0x2A80, 0xEA41,
-		0xEE01, 0x2EC0, 0x2F80, 0xEF41, 0x2D00, 0xEDC1, 0xEC81, 0x2C40,
-		0xE401, 0x24C0, 0x2580, 0xE541, 0x2700, 0xE7C1, 0xE681, 0x2640,
-		0x2200, 0xE2C1, 0xE381, 0x2340, 0xE101, 0x21C0, 0x2080, 0xE041,
-		0xA001, 0x60C0, 0x6180, 0xA141, 0x6300, 0xA3C1, 0xA281, 0x6240,
-		0x6600, 0xA6C1, 0xA781, 0x6740, 0xA501, 0x65C0, 0x6480, 0xA441,
-		0x6C00, 0xACC1, 0xAD81, 0x6D40, 0xAF01, 0x6FC0, 0x6E80, 0xAE41,
-		0xAA01, 0x6AC0, 0x6B80, 0xAB41, 0x6900, 0xA9C1, 0xA881, 0x6840,
-		0x7800, 0xB8C1, 0xB981, 0x7940, 0xBB01, 0x7BC0, 0x7A80, 0xBA41,
-		0xBE01, 0x7EC0, 0x7F80, 0xBF41, 0x7D00, 0xBDC1, 0xBC81, 0x7C40,
-		0xB401, 0x74C0, 0x7580, 0xB541, 0x7700, 0xB7C1, 0xB681, 0x7640,
-		0x7200, 0xB2C1, 0xB381, 0x7340, 0xB101, 0x71C0, 0x7080, 0xB041,
-		0x5000, 0x90C1, 0x9181, 0x5140, 0x9301, 0x53C0, 0x5280, 0x9241,
-		0x9601, 0x56C0, 0x5780, 0x9741, 0x5500, 0x95C1, 0x9481, 0x5440,
-		0x9C01, 0x5CC0, 0x5D80, 0x9D41, 0x5F00, 0x9FC1, 0x9E81, 0x5E40,
-		0x5A00, 0x9AC1, 0x9B81, 0x5B40, 0x9901, 0x59C0, 0x5880, 0x9841,
-		0x8801, 0x48C0, 0x4980, 0x8941, 0x4B00, 0x8BC1, 0x8A81, 0x4A40,
-		0x4E00, 0x8EC1, 0x8F81, 0x4F40, 0x8D01, 0x4DC0, 0x4C80, 0x8C41,
-		0x4400, 0x84C1, 0x8581, 0x4540, 0x8701, 0x47C0, 0x4680, 0x8641,
-		0x8201, 0x42C0, 0x4380, 0x8341, 0x4100, 0x81C1, 0x8081, 0x4040,
-	};
+        0x0000, 0xC0C1, 0xC181, 0x0140, 0xC301, 0x03C0, 0x0280, 0xC241,
+        0xC601, 0x06C0, 0x0780, 0xC741, 0x0500, 0xC5C1, 0xC481, 0x0440,
+        0xCC01, 0x0CC0, 0x0D80, 0xCD41, 0x0F00, 0xCFC1, 0xCE81, 0x0E40,
+        0x0A00, 0xCAC1, 0xCB81, 0x0B40, 0xC901, 0x09C0, 0x0880, 0xC841,
+        0xD801, 0x18C0, 0x1980, 0xD941, 0x1B00, 0xDBC1, 0xDA81, 0x1A40,
+        0x1E00, 0xDEC1, 0xDF81, 0x1F40, 0xDD01, 0x1DC0, 0x1C80, 0xDC41,
+        0x1400, 0xD4C1, 0xD581, 0x1540, 0xD701, 0x17C0, 0x1680, 0xD641,
+        0xD201, 0x12C0, 0x1380, 0xD341, 0x1100, 0xD1C1, 0xD081, 0x1040,
+        0xF001, 0x30C0, 0x3180, 0xF141, 0x3300, 0xF3C1, 0xF281, 0x3240,
+        0x3600, 0xF6C1, 0xF781, 0x3740, 0xF501, 0x35C0, 0x3480, 0xF441,
+        0x3C00, 0xFCC1, 0xFD81, 0x3D40, 0xFF01, 0x3FC0, 0x3E80, 0xFE41,
+        0xFA01, 0x3AC0, 0x3B80, 0xFB41, 0x3900, 0xF9C1, 0xF881, 0x3840,
+        0x2800, 0xE8C1, 0xE981, 0x2940, 0xEB01, 0x2BC0, 0x2A80, 0xEA41,
+        0xEE01, 0x2EC0, 0x2F80, 0xEF41, 0x2D00, 0xEDC1, 0xEC81, 0x2C40,
+        0xE401, 0x24C0, 0x2580, 0xE541, 0x2700, 0xE7C1, 0xE681, 0x2640,
+        0x2200, 0xE2C1, 0xE381, 0x2340, 0xE101, 0x21C0, 0x2080, 0xE041,
+        0xA001, 0x60C0, 0x6180, 0xA141, 0x6300, 0xA3C1, 0xA281, 0x6240,
+        0x6600, 0xA6C1, 0xA781, 0x6740, 0xA501, 0x65C0, 0x6480, 0xA441,
+        0x6C00, 0xACC1, 0xAD81, 0x6D40, 0xAF01, 0x6FC0, 0x6E80, 0xAE41,
+        0xAA01, 0x6AC0, 0x6B80, 0xAB41, 0x6900, 0xA9C1, 0xA881, 0x6840,
+        0x7800, 0xB8C1, 0xB981, 0x7940, 0xBB01, 0x7BC0, 0x7A80, 0xBA41,
+        0xBE01, 0x7EC0, 0x7F80, 0xBF41, 0x7D00, 0xBDC1, 0xBC81, 0x7C40,
+        0xB401, 0x74C0, 0x7580, 0xB541, 0x7700, 0xB7C1, 0xB681, 0x7640,
+        0x7200, 0xB2C1, 0xB381, 0x7340, 0xB101, 0x71C0, 0x7080, 0xB041,
+        0x5000, 0x90C1, 0x9181, 0x5140, 0x9301, 0x53C0, 0x5280, 0x9241,
+        0x9601, 0x56C0, 0x5780, 0x9741, 0x5500, 0x95C1, 0x9481, 0x5440,
+        0x9C01, 0x5CC0, 0x5D80, 0x9D41, 0x5F00, 0x9FC1, 0x9E81, 0x5E40,
+        0x5A00, 0x9AC1, 0x9B81, 0x5B40, 0x9901, 0x59C0, 0x5880, 0x9841,
+        0x8801, 0x48C0, 0x4980, 0x8941, 0x4B00, 0x8BC1, 0x8A81, 0x4A40,
+        0x4E00, 0x8EC1, 0x8F81, 0x4F40, 0x8D01, 0x4DC0, 0x4C80, 0x8C41,
+        0x4400, 0x84C1, 0x8581, 0x4540, 0x8701, 0x47C0, 0x4680, 0x8641,
+        0x8201, 0x42C0, 0x4380, 0x8341, 0x4100, 0x81C1, 0x8081, 0x4040,
+    };
 
 
 inline uint16_t crc16(const uint8_t data, uint16_t crc = 0)
 {
-	return (crc >> 8) ^ crc_table[(crc ^ data) & 0xFF];
+    return (crc >> 8) ^ crc_table[(crc ^ data) & 0xFF];
 }
 
 uint16_t crc16(const uint8_t* const data, size_t size, uint16_t initial_val = 0)
 {
-	const uint8_t* val = data;
-	uint16_t crc = initial_val;
-	while (size--)
-	{
-		crc = crc16(*val, crc);
-		++val;
-	}
-	return crc;
+    const uint8_t* val = data;
+    uint16_t crc = initial_val;
+    while (size--)
+    {
+        crc = crc16(*val, crc);
+        ++val;
+    }
+    return crc;
 }
 
 std::string getHex(const uint8_t * message, size_t size)
@@ -474,22 +474,22 @@ void HaierClimate::loop()
 
 void HaierClimate::getSerialData()
 {
+    bool prev_separator = false;
+    bool ignore_byte = false;
     while (available() > 0)
     {
         uint8_t val;
-		bool prev_separator = false;
-		bool ignore_byte = false;
         if (!read_byte(&val))
             break;
         if (currentPacket.size > 0) // Already found packet start
         {
-			// Skip 0x55 after 0xFF if it is not packets separator
-			ignore_byte = prev_separator && (val == SEPARATOR_POST_BYTE);
-			prev_separator = val == SEPARATOR_BYTE;
+            // Skip 0x55 after 0xFF if it is not packets separator
+            ignore_byte = (prev_separator && (val == SEPARATOR_POST_BYTE));
+            prev_separator = (val == SEPARATOR_BYTE);
             if (currentPacket.position < currentPacket.size)
             {
-				if (!ignore_byte)
-					currentPacket.buffer[currentPacket.position++] = val;
+                if (!ignore_byte)
+                    currentPacket.buffer[currentPacket.position++] = val;
                 if (currentPacket.position == 2) // Found crc indication flags
                 {
                     currentPacket.has_crc = (val & 0x40) != 0;
@@ -497,7 +497,7 @@ void HaierClimate::getSerialData()
                         currentPacket.size += 2;    // Make space for CRC
                 }
                 if (currentPacket.position < currentPacket.size - (currentPacket.has_crc ? 2 : 0))
-					// Not checking ignore_byte here, extra 0x55 should be included in checksum but not CRC
+                    // Not checking ignore_byte here, extra 0x55 should be included in checksum but not CRC
                     currentPacket.checksum += val;
             }
             if (currentPacket.position >= currentPacket.size)
@@ -680,63 +680,63 @@ void HaierClimate::sendFrameWithSubcommand(HaierProtocol::FrameType type, uint16
     }
     uint8_t buffer[MAX_FRAME_SIZE * 2] = {SEPARATOR_BYTE, SEPARATOR_BYTE};
     size_t pos = 2;
-	uint8_t checksum = 0;
-	uint16_t crc = CRC_INIT_VAL;
-	uint8_t header_buf[HEADER_SIZE] = {0};
+    uint8_t checksum = 0;
+    uint16_t crc = CRC_INIT_VAL;
+    uint8_t header_buf[HEADER_SIZE] = {0};
     HaierPacketHeader* header = (HaierPacketHeader*)header_buf;
     header->msg_length = msg_size;
     header->crc_present = mUseCrc ? 0x40 : 0x00;
     memset(header->reserved, 0, sizeof(header->reserved));
     header->frame_type = type;
 #define PROCESS_BYTE_FOR_PACKET(_buffer, _pos, _value) do { \
-				_buffer[_pos++] = _value; \
-				checksum += _value; \
-				if (mUseCrc) \
-					crc = crc16(_value, crc); \
-				if (_value == SEPARATOR_BYTE) \
-				{ \
-					_buffer[_pos++] = SEPARATOR_POST_BYTE; \
-					checksum += SEPARATOR_POST_BYTE; \
-					++frameSize; \
-				} \
-			} while (0)
-	for (int i = 0; i < sizeof(header_buf); i++)
-		PROCESS_BYTE_FOR_PACKET(buffer, pos, header_buf[i]);
+                _buffer[_pos++] = _value; \
+                checksum += _value; \
+                if (mUseCrc) \
+                    crc = crc16(_value, crc); \
+                if (_value == SEPARATOR_BYTE) \
+                { \
+                    _buffer[_pos++] = SEPARATOR_POST_BYTE; \
+                    checksum += SEPARATOR_POST_BYTE; \
+                    ++frameSize; \
+                } \
+            } while (0)
+    for (int i = 0; i < sizeof(header_buf); i++)
+        PROCESS_BYTE_FOR_PACKET(buffer, pos, header_buf[i]);
     if (subcommand != NO_SUBCOMMAND)
     {
-		uint8_t val = subcommand >> 8;
-		PROCESS_BYTE_FOR_PACKET(buffer, pos, val);
-		val = subcommand & 0xFF;
-		PROCESS_BYTE_FOR_PACKET(buffer, pos, val);
+        uint8_t val = subcommand >> 8;
+        PROCESS_BYTE_FOR_PACKET(buffer, pos, val);
+        val = subcommand & 0xFF;
+        PROCESS_BYTE_FOR_PACKET(buffer, pos, val);
     }
     if (data_size > 0)
     {
-		for (int i = 0; i < data_size; i++)
-			PROCESS_BYTE_FOR_PACKET(buffer, pos, data[i]);
+        for (int i = 0; i < data_size; i++)
+            PROCESS_BYTE_FOR_PACKET(buffer, pos, data[i]);
     }
 #undef PROCESS_BYTE_FOR_PACKET
     buffer[pos++] = checksum;
-	if (checksum == SEPARATOR_BYTE)
-	{
-		buffer[pos++] = SEPARATOR_POST_BYTE;
-		++frameSize;
-	}
+    if (checksum == SEPARATOR_BYTE)
+    {
+        buffer[pos++] = SEPARATOR_POST_BYTE;
+        ++frameSize;
+    }
     if (mUseCrc)
     {
-		uint8_t val = crc >> 8;
-		buffer[pos++] = val;
-		if (val == SEPARATOR_BYTE)
-		{
-			buffer[pos++] = SEPARATOR_POST_BYTE;
-			++frameSize;
-		}
-		val = crc & 0xFF;
-		buffer[pos++] = val;
-		if (val == SEPARATOR_BYTE)
-		{
-			buffer[pos++] = SEPARATOR_POST_BYTE;
-			++frameSize;
-		}
+        uint8_t val = crc >> 8;
+        buffer[pos++] = val;
+        if (val == SEPARATOR_BYTE)
+        {
+            buffer[pos++] = SEPARATOR_POST_BYTE;
+            ++frameSize;
+        }
+        val = crc & 0xFF;
+        buffer[pos++] = val;
+        if (val == SEPARATOR_BYTE)
+        {
+            buffer[pos++] = SEPARATOR_POST_BYTE;
+            ++frameSize;
+        }
     }
     write_array(buffer, frameSize);
     ESP_LOGD(TAG, "Message sent:%s", getHex(buffer, frameSize).c_str());
