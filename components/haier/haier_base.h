bc8c2af9859faa9f7e6a984b6886b28db6b2b87b
Pavlo Dudnytskyi <paveldudn@gmail.com>
2023-09-07 19:16:47 +0200

    Added valid_connection function to detect connection status

--- components/haier/haier_base.h
+++ components/haier/haier_base.h
@@ -45,6 +45,7 @@ class HaierClimateBase : public esphome::Component,
   void set_supported_modes(const std::set<esphome::climate::ClimateMode> &modes);
   void set_supported_swing_modes(const std::set<esphome::climate::ClimateSwingMode> &modes);
   void set_supported_presets(const std::set<esphome::climate::ClimatePreset> &presets);
+  bool valid_connection() { return this->protocol_phase_ >= ProtocolPhases::IDLE; };
   size_t available() noexcept override { return esphome::uart::UARTDevice::available(); };
   size_t read_array(uint8_t *data, size_t len) noexcept override {
     return esphome::uart::UARTDevice::read_array(data, len) ? len : 0;
