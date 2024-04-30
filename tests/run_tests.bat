8b76276f0a4e1c46f6ed1c117a68e74384b9ec84
Pavlo Dudnytskyi <pdudnytskyi@astrata.eu>
2024-04-30 11:45:11 +0200

    Tasks for vscode added

---
+++ tests/run_tests.bat
@@ -0,0 +1,27 @@
+@echo off
+for %%G in ( *.yaml ) do (
+	call :append_config_file_name %%G
+	if %errorlevel% NEQ 0 (
+		echo Error while building %%G
+		goto :eof
+	)
+)
+if /i "%1"=="show" (
+	echo List of tests %configs_list%
+	goto :eof
+)
+if /i "%1"=="wsl" (
+	wsl --cd "%cd%" /bin/sh -c "esphome compile %configs_list%"
+) else (
+	esphome compile %configs_list%
+)
+goto :eof
+
+:append_config_file_name
+	set config_file=%1
+	if "%config_file:~0,1%" NEQ "." (
+		set "configs_list=%configs_list% %config_file%"
+	) else (
+		echo Skipping %config_file%
+	)
+goto :eof
