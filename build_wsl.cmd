c126ac499199d7fdaa25d710c9930952edb582ac
Pavlo Dudnytskyi <pdudnytskyi@astrata.eu>
2024-07-08 08:25:46 +0200

    Fixed build_wsl.cmd

--- build_wsl.cmd
+++ build_wsl.cmd
@@ -1,6 +1,6 @@
 @echo off
 if [%1] == [] goto err
-wsl --cd "%cd%" /bin/sh -c "esphome compile %1 2>&1 | tee buildlog.txt"
+wsl -e sh -lc "esphome compile %1 2>&1 | tee buildlog.txt"
 goto:eof
 :err
 echo Please use with esphome configuration as parameter
