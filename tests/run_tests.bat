8b129f4de4860195ed959fb3860058b03e9b911b
Pavlo Dudnytskyi <pdudnytskyi@astrata.eu>
2024-07-17 18:53:07 +0200

    Sample configs updated

--- tests/run_tests.bat
+++ tests/run_tests.bat
@@ -6,21 +6,41 @@ for %%G in ( *.yaml ) do (
 		goto :eof
 	)
 )
+set "configs_list=%configs_list:~1%"
+set items_cout=0
+for %%i in (%configs_list%) do set /a cnt+=1
 if /i "%1"=="show" (
-	echo List of tests %configs_list%
+	echo List of tests: 
+	echo ----------------------------------------------------
+	for %%i in (%configs_list%) do (
+		echo     %%i
+	)
+	echo ----------------------------------------------------
+	echo Total: %cnt% items
 	goto :eof
 )
-if /i "%1"=="wsl" (
-	wsl --cd "%cd%" /bin/sh -c "~/.local/bin/esphome compile %configs_list%"
-) else (
-	esphome compile %configs_list%
+for %%i in (%configs_list%) do (
+	echo ----------------------------------------------------
+	echo Compiling: %%i
+	echo ----------------------------------------------------
+	if /i "%1"=="wsl" (
+	        wsl -e sh -lc "esphome compile %%i"
+	) else (
+		esphome compile %%i
+	)
+	if %ERRORLEVEL% NEQ 0 ( 
+		echo ----------------------------------------------------
+		echo Failed to build: %%i
+		echo ----------------------------------------------------
+		goto :eof
+	)
 )
 goto :eof
 
 :append_config_file_name
 	set config_file=%1
 	if "%config_file:~0,1%" NEQ "." (
-		set "configs_list=%configs_list% %config_file%"
+		set "configs_list=%configs_list%,%config_file%"
 	) else (
 		echo Skipping %config_file%
 	)
