c28ddd60fccb6031a07cacc2b1aff9fc3944c864
Pavlo Dudnytskyi <paveldudn@gmail.com>
2024-08-22 18:30:06 +0200

    Switches fix

--- components/haier/switch/health_mode.cpp
+++ components/haier/switch/health_mode.cpp
@@ -4,6 +4,9 @@ namespace esphome {
 namespace haier {
 
 void HealthModeSwitch::write_state(bool state) {
+  if (this->parent_->get_health_mode() != state) {
+    this->parent_->set_health_mode(state);
+  }
   this->publish_state(state);
 }
 
