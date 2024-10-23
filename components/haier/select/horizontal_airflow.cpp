ed871979c381d3e62edae4b6d9bce607d46efb6a
Pavlo Dudnytskyi <pdudnytskyi@astrata.eu>
2024-10-23 08:26:07 +0200

    Select for haier implementation changed to keep order of item names and values only in one place

--- components/haier/select/horizontal_airflow.cpp
+++ components/haier/select/horizontal_airflow.cpp
@@ -6,22 +6,13 @@ namespace haier {
 
 void HorizontalAirflowSelect::control(const std::string &value) {
   hon_protocol::HorizontalSwingMode state;
-  if (value == "Auto") {
-    state = hon_protocol::HorizontalSwingMode::AUTO;
-  } else if (value == "Max Left") {
-    state = hon_protocol::HorizontalSwingMode::MAX_LEFT;
-  } else if (value == "Left") {
-    state = hon_protocol::HorizontalSwingMode::LEFT;
-  } else if (value == "Center") {
-    state = hon_protocol::HorizontalSwingMode::CENTER;
-  } else if (value == "Right") {
-    state = hon_protocol::HorizontalSwingMode::RIGHT;
-  } else if (value == "Max Right") {
-    state = hon_protocol::HorizontalSwingMode::MAX_RIGHT;
-  } else {
+  static const std::vector<std::string> options = this->traits.get_options();
+  auto item_it = std::find(options.begin(), options.end(), value);
+  if (item_it == options.end()) {
     ESP_LOGE("haier", "Invalid horizontal airflow mode: %s", value.c_str());
     return;
   }
+  state = HORIZONTAL_SWING_MODES_ORDER[item_it - options.begin()];
   const esphome::optional<hon_protocol::HorizontalSwingMode> current_state = this->parent_->get_horizontal_airflow();
   if (!current_state.has_value() || (current_state.value() != state)) {
     this->parent_->set_horizontal_airflow(state);
--- components/haier/select/vertical_airflow.cpp
+++ components/haier/select/vertical_airflow.cpp
@@ -6,26 +6,13 @@ namespace haier {
 
 void VerticalAirflowSelect::control(const std::string &value) {
   hon_protocol::VerticalSwingMode state;
-  if (value == "Auto") {
-    state = hon_protocol::VerticalSwingMode::AUTO;
-  } else if (value == "Health Up") {
-    state = hon_protocol::VerticalSwingMode::HEALTH_UP;
-  } else if (value == "Max Up") {
-    state = hon_protocol::VerticalSwingMode::MAX_UP;
-  } else if (value == "Up") {
-    state = hon_protocol::VerticalSwingMode::UP;
-  } else if (value == "Center") {
-    state = hon_protocol::VerticalSwingMode::CENTER;
-  } else if (value == "Down") {
-    state = hon_protocol::VerticalSwingMode::DOWN;
-  } else if (value == "Max Down") {
-    state = hon_protocol::VerticalSwingMode::MAX_DOWN;
-  } else if (value == "Health Down") {
-    state = hon_protocol::VerticalSwingMode::HEALTH_DOWN;
-  } else {
+  static const std::vector<std::string> options = this->traits.get_options();
+  auto item_it = std::find(options.begin(), options.end(), value);
+  if (item_it == options.end()) {
     ESP_LOGE("haier", "Invalid vertical airflow mode: %s", value.c_str());
     return;
   }
+  state = VERTICAL_SWING_MODES_ORDER[item_it - options.begin()];
   const esphome::optional<hon_protocol::VerticalSwingMode> current_state = this->parent_->get_vertical_airflow();
   if (!current_state.has_value() || (current_state.value() != state)) {
     this->parent_->set_vertical_airflow(state);
